7ac91389e1e29df1ea2924d1fcff37f6b8600725
ArjunNarayanan <arjun.narayanan91@gmail.com>
2023-05-12 10:33:05 -0700

    updated figures

--- examples/figures_for_paper.jl
+++ examples/figures_for_paper.jl
@@ -4,19 +4,21 @@ using MeshPlotter
 TM = TriMeshGame
 MP = MeshPlotter
 
-function plot_mesh(mesh; fontsize = 30, vertex_size = 30)
-    fig = MP.plot_mesh(
+
+function plot_mesh(mesh; fontsize = 60, vertex_size = 70)
+    fig, ax = MP.plot_mesh(
         TM.active_vertex_coordinates(mesh), 
         TM.active_triangle_connectivity(mesh), 
         number_vertices = true, 
-        number_elements = true, 
+        # number_elements = true, 
         # internal_order = true,
         vertex_size = vertex_size, 
-        fontsize = fontsize)[1]
+        fontsize = fontsize)
 
     fig.tight_layout()    
     return fig
 end
+
 
 ##
 vertices = [0. 1. 1. 2.
@@ -25,7 +27,8 @@ connectivity = [1 2 4
                 1 4 3]
 
 mesh = TM.Mesh(vertices, connectivity')
-fig = plot_mesh(mesh, fontsize = 60, vertex_size = 70)
+fig = plot_mesh(mesh)
+fig
 fig.savefig("examples/paper_figures/flip-0.png")
 TM.edgeflip!(mesh, 1, 2)
 fig = plot_mesh(mesh, fontsize = 60, vertex_size = 70)
@@ -46,3 +49,20 @@ fig = plot_mesh(mesh, fontsize = 60, vertex_size = 70)
 fig.savefig("examples/paper_figures/interior-split-1.png")
 ##
 
+
+##
+vertices = [0. 0. 1. 2. 2. 3. 4. 4.
+            0. 2. 1. 0. 2. 1. 0. 2.]
+connectivity = [
+    1 1 3 3 3 4 6 6
+    3 4 5 4 6 7 8 7
+    2 3 2 6 5 6 5 8
+]
+mesh = TM.Mesh(vertices, connectivity)
+fig = plot_mesh(mesh, fontsize = 50, vertex_size = 50)
+fig.savefig("examples/paper_figures/collapse-0.png")
+TM.collapse!(mesh, 4, 2)
+TM.reindex!(mesh)
+fig = plot_mesh(mesh, fontsize = 50, vertex_size=50)
+fig.savefig("examples/paper_figures/collapse-1.png")
+##
